ac0e01c66f6cfb3cfd0fde0b80ff595d4830779a
Rémi Maréchal <remi.marechal@gmail.com>
2023-05-22 11:50:50 +0200

    Update configure-product.ps1

--- cd/01-windows-defender/configure-product.ps1
+++ cd/01-windows-defender/configure-product.ps1
@@ -1,2 +1,3 @@
+"disable windows defender" | Set-Content -Path "c:\windows-defender.txt"
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows Defender" -Name "DisableAntiSpyware" -Value "1"
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows Defender" -Name "DisableAntiVirus" -Value "1"
